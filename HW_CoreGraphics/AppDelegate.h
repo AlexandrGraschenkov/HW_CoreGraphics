d665660603010886ae36d2d4a6dbae121ccda988
Alexander <sharrikk@gmail.com>
2015-04-19 23:00:17 +0300

    add project

---
+++ HW_CoreGraphics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW_CoreGraphics
+//
+//  Created by Alexander on 19.04.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
